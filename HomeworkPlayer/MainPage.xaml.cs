c82b25c1e5c6528844d4bd440be24ef817dad7eb
lotsofone <552694725@qq.com>
2018-03-29 10:44:51 +0800

    Filepicker is working now.

--- HomeworkPlayer/MainPage.xaml.cs
+++ HomeworkPlayer/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -26,5 +28,16 @@ namespace HomeworkPlayer
         {
             this.InitializeComponent();
         }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            //picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+
+            picker.FileTypeFilter.Add(".mp4");
+            StorageFile file = await picker.PickSingleFileAsync();
+        }
+
     }
 }
